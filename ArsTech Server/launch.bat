328a4838c501f7bc184ca0bc66aa72139e6839db
qchronod <qchronod@gmail.com>
2013-08-07 12:22:43 -0500

    Project re-architected
    Re-architected project layout to reduce file duplication
    Should help with keeping server and client files consistant

--- ArsTech Server/launch.bat
+++
@@ -1,2 +0,0 @@
-java -server -XX:+UseConcMarkSweepGC -XX:+UseAdaptiveGCBoundary -XX:MaxGCPauseMillis=500 -XX:-UseGCOverheadLimit -XX:SurvivorRatio=12 -Xnoclassgc -XX:UseSSE=3 -Xincgc -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=7 -XX:+AggressiveOpts -XX:MaxPermSize=256m -Xms1G -Xmx8G -jar ArsTech.jar nogui
-pause
