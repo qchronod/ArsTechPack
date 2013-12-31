a20a91ea2f1d3a2cf0b8d5b641798ceb1d7d665a
qchronod <qchronod@gmail.com>
2013-12-30 20:18:41 -0600

    server launch fixes
    moved files to server base folder for easier setup
    updated launch files to point to new jar
    added modified dynmap config and world lists

--- server/launch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd minecraft
-java -server -XX:+UseConcMarkSweepGC -XX:+UseAdaptiveGCBoundary -XX:MaxGCPauseMillis=500 -XX:-UseGCOverheadLimit -XX:SurvivorRatio=12 -Xnoclassgc -XX:UseSSE=3 -Xincgc -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=7 -XX:+AggressiveOpts -XX:MaxPermSize=256m -Xms1G -Xmx8G -jar ArsTech.jar nogui
